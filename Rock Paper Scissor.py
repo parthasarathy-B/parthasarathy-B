f2a2cd6421641f3fb8e0f677bdf454bfcd4a1c4e
parthasarathy-B <81454132+parthasarathy-B@users.noreply.github.com>
2021-09-07 12:33:49 +0530

    Add files via upload

---
+++ Rock Paper Scissor.py
@@ -0,0 +1,50 @@
+import random
+
+ME=0
+BOT=0
+a= ['rock  ','sessor','paper ']
+b= ['rock  ','sessor','paper ']
+ans=('')
+
+print('* Press Enter button to Continue')
+print('* Press any other letter and Enter button to Exit')
+
+
+while(ans==''):
+    ans=input('               ')
+    if (ans !=''):
+        break
+    
+    bot=(random.choice(a))
+    me=(random.choice(b))
+    print('Bot :',bot,end='          ')
+    print('Me :',me)
+    print('            ',end='')
+    if ((bot==a[0] and me==b[1])or(bot==a[1] and me==b[2])or(bot==a[2] and me==b[0])):
+        BOT+=1
+        print('!YOU LOSE!')
+    elif(bot==me):
+        print('!DRAW!')
+    else:
+        ME+=1
+        print('!YOU WIN!')
+
+print('--------------------------------------------------------------------------------')
+print('ME  = ',ME)
+print('BOT = ',BOT)
+
+if ME>BOT:
+    print('''     ---------------------
+     | YOU WON THE MATCH |
+     ---------------------''')
+elif ME<BOT:
+    print('''     ----------------------
+     | YOU LOSE THE MATCH |
+     ----------------------''')
+else:
+    print('''     -----------------
+     | MATCH IS DRAW |
+     -----------------''')
+input()
+
+    
